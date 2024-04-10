9488a49e252d4c2b3f0e9a1b40197b37f03c7e53
Noam Muallem <noammu072@gmail.com>
2024-04-11 01:29:33 +0300

    added a custom 404 not found page

---
+++ src/app/not-found.tsx
@@ -0,0 +1,7 @@
+export default function NotFound() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-center text-2xl text-black">
+      404 | דף לא נמצא
+    </main>
+  );
+}
