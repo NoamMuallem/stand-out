ebc7da79b95df6bada1fc97a6a393cbd07b3d45c
Noam Muallem <noammu072@gmail.com>
2024-04-24 23:43:11 +0300

    suppressed errors

--- src/app/blog/[slug]/page.tsx
+++ src/app/blog/[slug]/page.tsx
@@ -1,3 +1,6 @@
+/* eslint-disable @typescript-eslint/no-unsafe-member-access */
+/* eslint-disable @typescript-eslint/no-explicit-any */
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
 import { NotionRenderer } from "@notion-render/client";
 import { notFound } from "next/navigation";
 import {
