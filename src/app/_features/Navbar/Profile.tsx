e13ac41ad049f554556e7cc9883c5906834c44c5
Noam Muallem <noammu072@gmail.com>
2024-05-06 15:59:45 +0300

    getting ready for a profile card

--- src/app/_features/Navbar/Profile.tsx
+++ src/app/_features/Navbar/Profile.tsx
@@ -7,8 +7,6 @@ import {
   DropdownMenuContent,
   DropdownMenuGroup,
   DropdownMenuItem,
-  DropdownMenuLabel,
-  DropdownMenuSeparator,
   DropdownMenuTrigger,
 } from "~/components/ui/dropdown-menu";
 
@@ -27,13 +25,11 @@ export function Profile() {
         </Avatar>
       </DropdownMenuTrigger>
       <DropdownMenuContent className="w-56">
-        <DropdownMenuLabel>My Account</DropdownMenuLabel>
-        <DropdownMenuSeparator />
         <DropdownMenuGroup>
           <Link href="/user-profile">
             <DropdownMenuItem>
               <User className="mr-2 h-4 w-4" />
-              <span>Profile</span>
+              <span>Account</span>
             </DropdownMenuItem>
           </Link>
         </DropdownMenuGroup>
