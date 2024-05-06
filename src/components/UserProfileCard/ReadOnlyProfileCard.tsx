f0a136e8e0547f2e29db8647e3c2ae95cc6fd0f7
Noam Muallem <noammu072@gmail.com>
2024-05-07 02:33:23 +0300

    a little better styling

--- src/components/UserProfileCard/ReadOnlyProfileCard.tsx
+++ src/components/UserProfileCard/ReadOnlyProfileCard.tsx
@@ -1,4 +1,5 @@
 import { useUser } from "@clerk/nextjs";
+import { Pencil } from "lucide-react";
 import { type UserProfile } from "~/types/trpcTypes";
 import { Avatar, AvatarFallback, AvatarImage } from "../ui/avatar";
 import { Button } from "../ui/button";
@@ -15,16 +16,22 @@ export const UserProfileCard = ({
   const { user } = useUser();
   return (
     <div className="w-full space-y-8 p-2">
-      {displayEdit ? <Button onClick={setEditMode}>Edit</Button> : null}
-      <div className="flex items-start justify-start gap-4">
-        <Avatar>
-          <AvatarImage src={user?.imageUrl} alt="User Profile" />
-          <AvatarFallback />
-        </Avatar>
-        <div className="flex flex-col items-start justify-start gap-1">
-          <span>{userProfile?.firstName}</span>
-          <span>{userProfile?.background}</span>
+      <div className="flex items-start justify-between gap-4">
+        <div className="flex items-start justify-start gap-4">
+          <Avatar>
+            <AvatarImage src={user?.imageUrl} alt="User Profile" />
+            <AvatarFallback />
+          </Avatar>
+          <div className="flex flex-col items-start justify-start gap-1">
+            <span>{userProfile?.firstName}</span>
+            <span>{userProfile?.background}</span>
+          </div>
         </div>
+        {displayEdit ? (
+          <Button variant="ghost" size="icon" onClick={setEditMode}>
+            <Pencil />
+          </Button>
+        ) : null}
       </div>
       <div>{`מספר ביקורות: ${userProfile?.amountOfRankings}`}</div>
       <div>{`ממוצע ביקורות: ${userProfile?.averageRanking}`}</div>
