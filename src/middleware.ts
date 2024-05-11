b927406480a5d99edc8be289418a322990ad31ca
Noam Muallem <noammu072@gmail.com>
2024-05-11 13:59:42 +0300

    fixed no direct langing in blog

--- src/middleware.ts
+++ src/middleware.ts
@@ -5,9 +5,11 @@ import { authMiddleware } from "@clerk/nextjs";
 // See https://clerk.com/docs/references/nextjs/auth-middleware for more information about configuring your middleware
 
 export default authMiddleware({
-  publicRoutes: ["/", "/contact", "about", "blog"],
+  publicRoutes: ["/", "/contact", "about", "/blog"],
 });
 
-export const config = {
-  matcher: ["/", "/(api|trpc)(.*)"],
-};
+//TODO: understand what this thing does and if I need it
+// this is why I did it: https://stackoverflow.com/questions/77108110/clerk-authmiddleware-is-not-being-used-even-though-it-is-in-my-middleware-ts-f
+// export const config = {
+//   matcher: ["/", "/(api|trpc)(.*)"],
+// };
